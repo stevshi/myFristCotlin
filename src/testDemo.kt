2c66092f557098141c283294fe38e073516a542e
stevshi <hx.shi@bster.com.cn>
2021-09-17 14:22:31 +0800

    test

--- src/testDemo.kt
+++ src/testDemo.kt
@@ -1,5 +1,5 @@
 fun main (args:Array<String>){
     println("Hello World!!!!!!!");
     println("finish git!");
-    println("finish git!");    println("finish git!"); println("finish git!");
+    println("finish git!");    println("finish git!"); println("finish git!"); println("finish git!");
 }
